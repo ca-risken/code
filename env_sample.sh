4d64c457edf43a99439ca8acd5fc960c3ffb1ab8
gassara-kys <ogasawara_kiyoshi@cybernagent>
2022-02-25 11:36:00 +0900

    rm: env

--- env_sample.sh
+++ env_sample.sh
@@ -6,27 +6,3 @@ export GITHUB_TOKEN="your-token"
 
 # GO
 export GOPRIVATE="github.com/CyberAgent/*,github.com/ca-risken/*"
-
-# DB
-export DB_MASTER_HOST="db"
-export DB_MASTER_USER="hoge"
-export DB_MASTER_PASSWORD="moge"
-export DB_SLAVE_HOST="db"
-export DB_SLAVE_USER="hoge"
-export DB_SLAVE_PASSWORD="moge"
-export DB_LOG_MODE="false"
-
-# CODE
-export PORT="10001"
-export DATA_KEY="12345678901234567890123456789012"
-export AWS_REGION="ap-northeast-1"
-export SQS_ENDPOINT="http://sqs:9324"
-export GITLEAKS_QUEUE_NAME="code-gitleaks"
-export GITLEAKS_QUEUE_URL="http://sqs:9324/queue/code-gitleaks"
-export GITHUB_DEFAULT_TOKEN="your-token"
-export LIMIT_REPOSITORY_SIZE_KB=350000 # 350 MB
-
-# gRPC server
-export FINDING_SVC_ADDR="finding:8001"
-export ALERT_SVC_ADDR="alert:8004"
-export CODE_SVC_ADDR="code:10001"
