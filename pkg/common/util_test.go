3d4d6c17d6ac9e0fe7a36da65ab93daf89ccc99d
Senk <44151180+senk8@users.noreply.github.com>
2025-05-08 15:55:47 +0900

    add test for CutString and CreateCloneDir

--- pkg/common/util_test.go
+++ pkg/common/util_test.go
@@ -1,6 +1,7 @@
 package common
 
 import (
+	"os"
 	"reflect"
 	"testing"
 
@@ -202,6 +203,100 @@ func TestFilterByVisibility(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := FilterByVisibility(tt.args.repos, tt.args.scanPublic, tt.args.scanInternal, tt.args.scanPrivate); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("filterByRepository() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestCutString(t *testing.T) {
+	type args struct {
+		input string
+		cut   int
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "Short string, no cut needed",
+			args: args{
+				input: "Hello",
+				cut:   10,
+			},
+			want: "Hello",
+		},
+		{
+			name: "Exact cut length",
+			args: args{
+				input: "Hello, World!",
+				cut:   5,
+			},
+			want: "Hello ...",
+		},
+		{
+			name: "Long string, cut applied",
+			args: args{
+				input: "This is a long string that needs to be cut",
+				cut:   10,
+			},
+			want: "This is a  ...",
+		},
+		{
+			name: "Empty string",
+			args: args{
+				input: "",
+				cut:   5,
+			},
+			want: "",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := CutString(tt.args.input, tt.args.cut); got != tt.want {
+				t.Errorf("CutString() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestCreateCloneDir(t *testing.T) {
+	type args struct {
+		repoName string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "Valid repo name",
+			args: args{
+				repoName: "testRepo",
+			},
+			wantErr: false,
+		},
+		{
+			name: "Empty repo name",
+			args: args{
+				repoName: "",
+			},
+			wantErr: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			dir, err := CreateCloneDir(tt.args.repoName)
+			if err == nil && tt.wantErr {
+				t.Errorf("CreateCloneDir() error = %v, wantErr %v", err, tt.wantErr)
+			}
+			if err != nil && !tt.wantErr {
+				t.Errorf("CreateCloneDir() error = %v, wantErr %v", err, tt.wantErr)
+			}
+			if err == nil {
+				os.RemoveAll(dir)
 			}
 		})
 	}
