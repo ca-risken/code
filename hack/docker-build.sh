4ad7c05fc366b1f0461a9f9a64f9546109ce8f93
han-ekiken <han_ekiken@cyberagent.co.jp>
2025-11-18 14:55:12 +0900

    [mod]linter error

--- hack/docker-build.sh
+++ hack/docker-build.sh
@@ -6,12 +6,11 @@ if [ "${IMAGE_PREFIX}" = "" ]; then
   IMAGE_PREFIX=default_prefix
 fi
 
-# Determine build context: if we're in code directory, go to parent; otherwise use current dir
-if [ -d "../datasource-api" ] && [ -d "../code" ]; then
-  # We're in code directory, build from parent
+# Build from parent directory to match Dockerfile expectations (COPY code/..., COPY datasource-api/...)
+# If we're in code directory, go to parent; otherwise assume we're already in parent
+if [ -d "../code" ] && [ "$(basename $(pwd))" = "code" ]; then
   cd ..
-  docker build ${BUILD_OPT} -t ${IMAGE_PREFIX}/${TARGET}:${IMAGE_TAG} -f code/dockers/${TARGET}/Dockerfile .
-else
-  # Build from current directory (assumes parent context)
-  docker build ${BUILD_OPT} -t ${IMAGE_PREFIX}/${TARGET}:${IMAGE_TAG} -f dockers/${TARGET}/Dockerfile .
 fi
+
+# Build with parent context (required for Dockerfile COPY commands)
+docker build ${BUILD_OPT} -t ${IMAGE_PREFIX}/${TARGET}:${IMAGE_TAG} -f code/dockers/${TARGET}/Dockerfile .
