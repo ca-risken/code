c1a36922b965081a90bebf7dffe7f5a8c028810f
han-ekiken <han_ekiken@cyberagent.co.jp>
2025-11-18 21:38:17 +0900

    [del]不要なコメントアウト

--- hack/docker-build.sh
+++ hack/docker-build.sh
@@ -6,5 +6,4 @@ if [ "${IMAGE_PREFIX}" = "" ]; then
   IMAGE_PREFIX=default_prefix
 fi
 
-# Build with the current (code) directory as context to avoid sending unnecessary files
 docker build ${BUILD_OPT} -t ${IMAGE_PREFIX}/${TARGET}:${IMAGE_TAG} -f dockers/${TARGET}/Dockerfile .
