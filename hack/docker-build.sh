6b57aa6455053c3e16076887d17bb28338598ff9
han-ekiken <han_ekiken@cyberagent.co.jp>
2025-11-18 16:21:13 +0900

    [mod]Dockerfile

--- hack/docker-build.sh
+++ hack/docker-build.sh
@@ -6,11 +6,5 @@ if [ "${IMAGE_PREFIX}" = "" ]; then
   IMAGE_PREFIX=default_prefix
 fi
 
-# Build from parent directory to match Dockerfile expectations (COPY code/..., COPY datasource-api/...)
-# If we're in code directory, go to parent; otherwise assume we're already in parent
-if [ -d "../code" ] && [ "$(basename $(pwd))" = "code" ]; then
-  cd ..
-fi
-
-# Build with parent context (required for Dockerfile COPY commands)
-docker build ${BUILD_OPT} -t ${IMAGE_PREFIX}/${TARGET}:${IMAGE_TAG} -f code/dockers/${TARGET}/Dockerfile .
+# Build with the current (code) directory as context to avoid sending unnecessary files
+docker build ${BUILD_OPT} -t ${IMAGE_PREFIX}/${TARGET}:${IMAGE_TAG} -f dockers/${TARGET}/Dockerfile .
