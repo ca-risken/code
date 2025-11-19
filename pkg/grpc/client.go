2a03510ba474eaa019ab1c9657e908d606977fc9
han-ekiken <han_ekiken@cyberagent.co.jp>
2025-11-19 14:51:54 +0900

    [mod]

--- pkg/grpc/client.go
+++ pkg/grpc/client.go
@@ -1,7 +1,7 @@
 package grpc
 
 import (
-	"context"
+	"time"
 
 	"github.com/ca-risken/core/proto/alert"
 	"github.com/ca-risken/core/proto/finding"
@@ -10,34 +10,40 @@ import (
 	"google.golang.org/grpc/credentials/insecure"
 )
 
-func NewFindingClient(ctx context.Context, svcAddr string) (finding.FindingServiceClient, error) {
-	conn, err := getGRPCConn(ctx, svcAddr)
+func NewFindingClient(svcAddr string) (finding.FindingServiceClient, error) {
+	conn, err := getGRPCConn(svcAddr)
 	if err != nil {
 		return nil, err
 	}
 	return finding.NewFindingServiceClient(conn), nil
 }
 
-func NewAlertClient(ctx context.Context, svcAddr string) (alert.AlertServiceClient, error) {
-	conn, err := getGRPCConn(ctx, svcAddr)
+func NewAlertClient(svcAddr string) (alert.AlertServiceClient, error) {
+	conn, err := getGRPCConn(svcAddr)
 	if err != nil {
 		return nil, err
 	}
 	return alert.NewAlertServiceClient(conn), nil
 }
 
-func NewCodeClient(ctx context.Context, svcAddr string) (code.CodeServiceClient, error) {
-	conn, err := getGRPCConn(ctx, svcAddr)
+func NewCodeClient(svcAddr string) (code.CodeServiceClient, error) {
+	conn, err := getGRPCConn(svcAddr)
 	if err != nil {
 		return nil, err
 	}
 	return code.NewCodeServiceClient(conn), nil
 }
 
-func getGRPCConn(ctx context.Context, addr string) (*grpc.ClientConn, error) {
+func getGRPCConn(addr string) (*grpc.ClientConn, error) {
 	// Note: OpenTelemetry interceptor is not added to avoid trace overhead
 	// due to high frequency of gRPC calls
-	conn, err := grpc.NewClient(addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	// Connection timeout is set to 3 seconds to prevent indefinite connection attempts
+	conn, err := grpc.NewClient(addr,
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+		grpc.WithConnectParams(grpc.ConnectParams{
+			MinConnectTimeout: 3 * time.Second,
+		}),
+	)
 	if err != nil {
 		return nil, err
 	}
