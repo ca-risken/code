93e8129fbf2a8ba59d5b9d3cee8ff7fed03c4520
han-ekiken <han_ekiken@cyberagent.co.jp>
2025-11-19 17:56:15 +0900

    [add]ctx

--- pkg/grpc/client.go
+++ pkg/grpc/client.go
@@ -1,51 +1,73 @@
 package grpc
 
 import (
+	"context"
 	"time"
 
 	"github.com/ca-risken/core/proto/alert"
 	"github.com/ca-risken/core/proto/finding"
 	"github.com/ca-risken/datasource-api/proto/code"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/connectivity"
 	"google.golang.org/grpc/credentials/insecure"
 )
 
-func NewFindingClient(svcAddr string) (finding.FindingServiceClient, error) {
-	conn, err := getGRPCConn(svcAddr)
+func NewFindingClient(ctx context.Context, svcAddr string) (finding.FindingServiceClient, error) {
+	conn, err := getGRPCConn(ctx, svcAddr)
 	if err != nil {
 		return nil, err
 	}
 	return finding.NewFindingServiceClient(conn), nil
 }
 
-func NewAlertClient(svcAddr string) (alert.AlertServiceClient, error) {
-	conn, err := getGRPCConn(svcAddr)
+func NewAlertClient(ctx context.Context, svcAddr string) (alert.AlertServiceClient, error) {
+	conn, err := getGRPCConn(ctx, svcAddr)
 	if err != nil {
 		return nil, err
 	}
 	return alert.NewAlertServiceClient(conn), nil
 }
 
-func NewCodeClient(svcAddr string) (code.CodeServiceClient, error) {
-	conn, err := getGRPCConn(svcAddr)
+func NewCodeClient(ctx context.Context, svcAddr string) (code.CodeServiceClient, error) {
+	conn, err := getGRPCConn(ctx, svcAddr)
 	if err != nil {
 		return nil, err
 	}
 	return code.NewCodeServiceClient(conn), nil
 }
 
-func getGRPCConn(addr string) (*grpc.ClientConn, error) {
+func getGRPCConn(ctx context.Context, addr string) (*grpc.ClientConn, error) {
 	// Note: OpenTelemetry interceptor is not added to avoid trace overhead
 	// due to high frequency of gRPC calls
-	// Connection timeout is set to 3 seconds to prevent indefinite connection attempts
-	conn, err := grpc.NewClient(addr,
-		grpc.WithTransportCredentials(insecure.NewCredentials()),
-		grpc.WithConnectParams(grpc.ConnectParams{
-			MinConnectTimeout: 3 * time.Second,
-		}),
-	)
+	conn, err := grpc.NewClient(addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
 		return nil, err
 	}
+	if ctx == nil {
+		ctx = context.Background()
+	}
+	connectCtx := ctx
+	cancel := func() {}
+	if _, ok := ctx.Deadline(); !ok {
+		connectCtx, cancel = context.WithTimeout(ctx, 3*time.Second)
+	}
+	defer cancel()
+	conn.Connect()
+	if err := waitForReady(connectCtx, conn); err != nil {
+		_ = conn.Close()
+		return nil, err
+	}
 	return conn, nil
+}
+
+func waitForReady(ctx context.Context, conn *grpc.ClientConn) error {
+	for {
+		state := conn.GetState()
+		if state == connectivity.Ready {
+			return nil
+		}
+		if !conn.WaitForStateChange(ctx, state) {
+			return ctx.Err()
+		}
+	}
 }
